6014b41407ab8a05ee85fbff93ff1bdb7378deed
bk7v <46638986+bk7v@users.noreply.github.com>
2019-01-18 02:35:09 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,5 +1,9 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const fs = require('fs');
+const moment = require('moment');
+const jimp = require('jimp');
+const Canvas = require('canvas');
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
