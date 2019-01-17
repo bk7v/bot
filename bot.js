5536a582f1ee770aecf2e2188f6c63f4cee0b372
bk7v <46638986+bk7v@users.noreply.github.com>
2019-01-18 02:39:32 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1174,11 +1174,7 @@ client.on('guildMemberAdd', member => {
 
 
 
-const client = new Discord.Client();
-const fs = require('fs');
-const moment = require('moment');
-const jimp = require('jimp');
-const Canvas = require('canvas');
+
 
 client.on('guildMemberAdd', member => {
      const welcomer =  member.guild.channels.find('name', 'welcome');
