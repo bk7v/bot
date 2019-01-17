d19a6b1ffa0e6ead35324daf58127f4133878eb6
bk7v <46638986+bk7v@users.noreply.github.com>
2019-01-18 02:32:08 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1169,7 +1169,7 @@ client.on('guildMemberAdd', member => {
 
 
 
-const Discord = require('discord.js');
+
 const client = new Discord.Client();
 const fs = require('fs');
 const moment = require('moment');
